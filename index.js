e0864119c62b521d8d5571883d742b1d57ca4afa
jtrhb <hubing1988@gmail.com>
2016-08-07 16:03:20 +0800

    2016-08-07

--- index.js
+++ index.js
@@ -10,85 +10,137 @@ var sql_host = 'localhost';
 var sql_user = 'root';
 var sql_psd = 'root';
 var sql_db = 'test';
-var sql_conn = {connectionLimit:sql_cLimit, host:sql_host, user:sql_user, password:sql_psd, database:sql_db};
+var sql_multiple = true;
+var sql_conn = {connectionLimit:sql_cLimit, host:sql_host, user:sql_user, password:sql_psd, database:sql_db, multipleStatements:sql_multiple};
 var pool = mysql.createPool(sql_conn);
-var jsonResponse, minTickCount, chartData, allConnections, lastTickData;
+var jsonResponse, minTickCount, chartData, allConnections = [], lastTickData;
+var bid = 0, ask = 0, spread = 0;
 
 pool.getConnection(function(err,connection) {
   if(err) {
     console.error('error connecction;'+err.stack);
     return;
   } else {
-    var sql_stmt = 'SELECT * FROM `forexm1`';
+    var realtimeUpdate = function() {
+      connection.query(realtimesql_stmt, function(err, rows, fields) {
+        if(err) {
+          throw err;
+        } else {
+          connection.release();
+          if(bid != Number(rows[0]['Bid']) || ask != Number(rows[0]['Ask'])) {
+            bid = rows[0]['Bid'];
+            ask = rows[0]['Ask'];
+            realtimeUpdate;
+          }
+        }
+      });
+    }
+    var sql_stmt = 'SELECT * FROM `forexm1`; SELECT * FROM `forexsec`';
+    var realtimesql_stmt = 'SELECT * FROM `forexrealtime`';
     connection.query(sql_stmt, function(err, rows, fields) {
       if(err) {
         throw err;
       } else {
-        minTickCount = rows.length;
-        chartData = rows;
-        jsonResponse = JSON.stringify(rows);
+        minTickCount = rows[0].length;
+        chartData = rows[0];
+        lastTickData = rows[1];
+        chartData.push(lastTickData[0]);
+        //jsonResponse = JSON.stringify(rows);
         connection.release();
-        getSecUpdate();
-        checkMinUpdate();
+        tickUpdate();
+        realtimeUpdate();
       }
     });
   }
 });
 
-function checkMinUpdate() {
-  var minUpdateStmt = 'SELECT ID FROM `forexm1` DESC LIMIT 0,1';
+function tickUpdate() {
   pool.getConnection(function(err, connection) {
-    connection.query(minUpdateStmt, function(err, rows, fields) {
+    var secUpdateStmt = 'SELECT * FROM `forexsec`';
+    var checkMinUpdateStmt = 'SELECT `ID` FROM `forexm1` ORDER BY `ID` DESC LIMIT 0,1';
+    var minUpdateStmt = 'SELECT * FROM `forexm1` ORDER BY `ID` DESC LIMIT 0,1';
+    var jointStmt1 = 'SELECT * FROM `forexsec`; SELECT `ID` FROM `forexm1` ORDER BY `ID` DESC LIMIT 0,1';
+    var jointStmt2 = 'SELECT * FROM `forexsec`; SELECT * FROM `forexm1` ORDER BY `ID` DESC LIMIT 0,1';
+    connection.query(jointStmt1, function(err, rows, fields) {
       if(err) {
         throw err;
       } else {
-        connection.release();
-        if(rows[0]['ID'] > minTickCount) {
-          getMinUpdate;
+        if(rows[1][0]['ID'] > minTickCount) {
+          connection.query(jointStmt2, function(err, rows, fields) {
+            if(err) {
+              throw err;
+            } else {
+              connection.release();
+              lastTickData = rows[0];
+              chartData[chartData.length - 1] = rows[1][0];
+              chartData.push(lastTickData[0]);
+              minTickCount += 1;
+              if(allConnections.length) {
+                io.emit('min chart update', {min:JSON.stringify(rows[1]), sec:JSON.stringify(lastTickData)});
+              }
+              setTimeout(tickUpdate,1000);
+            }
+          });
         } else {
-          setTimeout(checkMinUpdate, 1000);
-        }
-      }
-    });
-  });  
-}
-
-function getMinUpdate() {
-  var minUpdateStmt = 'SELECT * FROM `forexm1` DESC LIMIT 0,1';
-  pool.getConnection(function(err, connection) {
-    connection.query(minUpdateStmt, function(err, rows, fields) {
-      if(err) {
-        throw err;
-      } else {
-        chartData.push(rows);
-        connection.release();
-        if(allConnections.length) {
-          io.emit('min chart update', JSON.stringify(rows));
-          setTimeout(checkMinUpdate,60000);
-        } 
-      }
-    });
-  }); 
-}
-
-function getSecUpdate() {
-  var secUpdateStmt = 'SELECT * FROM `forexsec`';
-  pool.getConnection(function(err, connection) {
-    connection.query(secUpdateStmt, function(err, rows, fields) {
-      if(err) {
-        throw err;
-      } else {
-        lastTickData = rows;
-        connection.release();
-        if(allConnections.length) {
-          io.emit('sec chart update', JSON.stringify(rows));
-          setTimeout(getSecUpdate(), 1000);
+          lastTickData = rows[0];
+          chartData[chartData.length - 1] = lastTickData[0];
+          if(allConnections.length) {
+            io.emit('sec chart update', JSON.stringify(lastTickData));
+          }
+          connection.release();
+          setTimeout(tickUpdate,1000);
         }
       }
     });
   });
 }
 
+/*
+function checkMinUpdate(connection) {
+  var minUpdateStmt = 'SELECT `ID` FROM `forexm1` ORDER BY `ID` DESC LIMIT 0,1';
+  connection.query(minUpdateStmt, function(err, rows, fields) {
+    if(err) {
+      throw err;
+    } else {
+      if(rows[0]['ID'] > minTickCount) {
+        //getMinUpdate;
+      } else {
+        setTimeout(checkMinUpdate(connection), 1000);
+      }
+    }
+  }); 
+}
+
+function getMinUpdate(connection) {
+  var minUpdateStmt = 'SELECT * FROM `forexm1` ORDER BY `ID` DESC LIMIT 0,1';
+  connection.query(minUpdateStmt, function(err, rows, fields) {
+    if(err) {
+      throw err;
+    } else {
+      chartData.push(rows);
+      if(allConnections.length) {
+        io.emit('min chart update', JSON.stringify(rows));
+        setTimeout(checkMinUpdate(connection),60000);
+      } 
+    }
+  });
+}
+
+function getSecUpdate(connection) {
+  var secUpdateStmt = 'SELECT * FROM `forexsec`';
+  connection.query(secUpdateStmt, function(err, rows, fields) {
+    if(err) {
+      throw err;
+    } else {
+      lastTickData = rows;
+      if(allConnections.length) {
+        io.emit('sec chart update', JSON.stringify(rows));
+        setTimeout(getSecUpdate(connection), 1000);
+      }
+    }
+  });
+}
+*/
 
 app.use(express.static(path.join(__dirname, 'public'))); //设置环境变量
 app.get('/', function(req, res){
@@ -98,6 +150,7 @@ app.get('/', function(req, res){
 io.on('connection', function(socket){
   console.log('a user connected');
   allConnections.push(socket);
+  chartData;
   socket.emit('init chart update', JSON.stringify(chartData));
   socket.on('chat message', function(msg){
       io.emit('chat message', msg);
@@ -107,10 +160,6 @@ io.on('connection', function(socket){
   });
 });
 
-setInterval(function() {
-  io.emit('chat message', '');
-},1000);
-
-http.listen(80, function(){
-  console.log('listening on *:80');
+http.listen(911, function(){
+  console.log('listening on *:911');
 });
